44e11f269cfb3525914004a3bdd16aa114837798
pelebl <pelebl@gmail.com>
2013-09-24 19:12:03 +0800

    Update home page
    updated page link

--- findnews.user.js
+++ findnews.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @id             findchange@userscript.xiaja.com
-// @name           findchangelink
+// @name           FindNews
 // @version        1.0
 // @namespace      userscript.xiaja.com
 // @author         pele
 // @description    Find news which you didn't read
 // @include        http://www.ifeng.com/
 // @include        http://www.sina.com.cn/
-// @homepageURL    https://userscripts.org/scripts/show/165951
-// @updateURL      https://userscripts.org/scripts/source/165951.meta.js
-// @downloadURL    https://userscripts.org/scripts/source/165951.user.js
+// @homepageURL    https://github.com/pelebl/FindNews/
+// @updateURL      https://raw.github.com/pelebl/FindNews/master/findnews.user.js
+// @downloadURL    https://raw.github.com/pelebl/FindNews/master/findnews.user.js
 // @run-at         document-end
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
 // @require        https://raw.github.com/bbarakaci/fixto/master/dist/fixto.min.js
