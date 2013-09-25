4dd5fe554b4f73ed50d2078581c5c7d41479414b
pelebl <pelebl@gmail.com>
2013-09-25 10:20:58 +0800

    Add flout menu

--- findnews.user.js
+++ findnews.user.js
@@ -1,19 +1,20 @@
 // ==UserScript==
-// @id             findchange@userscript.xiaja.com
-// @name           FindNews
-// @version        1.0
+// @name           新闻变更
+// @version        1.0.0
 // @namespace      userscript.xiaja.com
-// @author         pele
-// @description    Find news which you didn't read
+// @author         pelebl@gmail.com
+// @description    帮你找到最新的新闻
 // @include        http://www.ifeng.com/
 // @include        http://www.sina.com.cn/
+// @include        http://www.163.com/
+// @include        http://www.sohu.com/
 // @homepageURL    https://github.com/pelebl/FindNews/
 // @updateURL      https://raw.github.com/pelebl/FindNews/master/findnews.user.js
 // @downloadURL    https://raw.github.com/pelebl/FindNews/master/findnews.user.js
 // @run-at         document-end
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
-// @require        https://raw.github.com/bbarakaci/fixto/master/dist/fixto.min.js
 // ==/UserScript==
+
 
 var arr = [];
 var links = '';
@@ -27,8 +28,7 @@ $.each($('a'), function(index, obj){
         var tmpVal = localStorage[key];
         if(tmpVal === undefined || tmpVal != value){
             localStorage[key] = value;
-            $(obj).attr('style','color:green;text-decoration:line-through;');
-            
+            $(obj).attr('style','color:green;text-decoration:line-through;');            
             links = links + '<li><a href="' +value + '" >' + key + '</li>';
         }
     }   
@@ -36,6 +36,16 @@ $.each($('a'), function(index, obj){
 );
 
 links = '<div id="FindNews"><ul>' + links + '</ul></div>';
+
 $('body').append(links);
 
-$('#FindNews').fixTo('body');
+GM_addStyle( 
+    ' #FindNews {             ' +
+    '    background: white;     ' +
+    '    border: 2px solid red; ' +
+    '    padding: 4px;          ' +
+    '    position: fixed;    ' +
+    '    top: 150px; right: 8px;   ' +
+    '    max-width: 400px;      ' +
+    ' } '
+);
