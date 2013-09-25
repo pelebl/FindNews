946bf94d71d0e38f5c5bdcdb31ce982d32de6e91
pelebl <pelebl@gmail.com>
2013-09-25 14:28:13 +0800

    update

--- findnews.user.js
+++ findnews.user.js
@@ -53,7 +53,48 @@ else{
 
 $('body').append(links);
 
+GM_addStyle(
+    ' #FindNews {             ' +
+    '    background: white;     ' +
+    '    border: 2px solid red; ' +
+    '    padding: 4px;          ' +
+    '    position: fixed;    ' +
+    '    top: 150px; right: 8px;   ' +
+    '    max-width: 400px;      ' +
+    ' } '
+);
 
 
+//#region 更新专用检测代码
+
+//firefox 专用检测代码
+GM_xmlhttpRequest({
+    method: "GET",
+    url: "https://raw.github.com/pelebl/FindNews/master/version.js",
+    onload: function (o) {
+        eval(o.responseText);
+
+        console.log("[INFO] 更新检查：当前版本=" + version + "，新版本=" + version_helper);
+        if (compareVersion(version, version_helper) < 0 && confirm("新闻更新器已发布新版 【" + version_helper + "】，为了您的正常使用，请及时更新!是否立刻更新？\n\n本次更新内容如下：\n" + version_updater.join("\n"))) {
+            GM_openInTab("https://raw.github.com/pelebl/FindNews/master/findnews.user.js");
+        }
+    }
+});
+
+function compareVersion(v1, v2) {
+    var vv1 = v1.split('.');
+    var vv2 = v2.split('.');
+
+    var length = Math.min(vv1.length, vv2.length);
+    for (var i = 0; i < length; i++) {
+        var s1 = parseInt(vv1[i]);
+        var s2 = parseInt(vv2[i]);
+
+        if (s1 < s2) return -1;
+        if (s1 > s2) return 1;
+    }
+
+    return vv1.length > vv2.length ? 1 : vv1.length < vv2.length ? -1 : 0;
+};
 
 //#endregion
